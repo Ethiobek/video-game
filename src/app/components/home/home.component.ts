8b0f7539b7ef8269dd7c78a91ed1b967cfc75904
Bereket Zergaw <arkishasoftware@gmail.com>
2022-01-16 22:24:04 +0300

    Load games to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
+import { APIResponse ,Game} from './../../models';
+import { HttpService } from './../../services/http.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public sort:string='';
-  constructor() { }
+  public games: Array<Game>=[];
+  constructor(
+    private httpService:HttpService,
+    private activatedRoute:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params:Params)=>{
+      this.searchGames('metacrit',params['game-search']);
+    });
+  }
+  searchGames(sort: string, search?: string) {
+    this.httpService
+    .getGameList(sort,search)
+    .subscribe((gameList:APIResponse<Game>)=>{
+      this.games=gameList.results;
+      console.log(this.games);
+    })
   }
 
 }
