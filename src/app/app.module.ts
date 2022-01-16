4ef03f2d379fc76cd8266b164808d2abb62b5e6d
Bereket Zergaw <arkishasoftware@gmail.com>
2022-01-16 19:19:01 +0300

    Search bar and home component first step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { GaugeModule } from 'angular-gauge';
+
+// Import material modules
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import {  MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { HomeComponent } from './components/home/home.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatTabsModule,MatIconModule,MatFormFieldModule,MatSelectModule  ,HttpClientModule ,GaugeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
